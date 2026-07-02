9e3ac5ccdf88974ef72881b0db12e2f08b870dc3
Gaurav-Kurude <gauravkurude.sitmech@gmail.com>
2026-07-02 12:49:06 +0530

    playing with routs

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+
+const books = [
+    { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 }
+]
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express server.");
+});
+
+app.post("/books", (req, res)=> {
+    const newBook = req.body;
+
+    if(!newBook.title || !newBook.author || !newBook.year) {
+        res.status(400).json({ error: "Title, author, and year are required" });
+    } else {
+        books.push(newBook);
+        res.status(201).json({ message: "Book added successfully", book: newBook });
+    }
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
